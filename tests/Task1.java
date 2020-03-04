b8cc2fac2ef7f5d80dcdb713452992dec02e0e72
thomson008 <tkhorszczaruk@gmail.com>
2020-03-04 14:30:26 +0000

    found 6 bugs

--- tests/Task1.java
+++ tests/Task1.java
@@ -1,3 +1,50 @@
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import org.junit.Test;
+import st1920.automaton.RegExpMatcher;
+
 public class Task1 {
+	@Test
+	public void email() {
+		String test = "user@email.com";
+		String regex = "([a-zA-Z0-9])+\\@([a-zA-Z0-9])+\\.([a-z]){2,3}";
+		assertTrue(RegExpMatcher.matches(test, regex));
+	}
+	
+	@Test
+	public void regexEmpty() {
+		String test = "1234";
+		String regex = "";
+		assertFalse(RegExpMatcher.matches(test, regex));
+	}
+	
+	@Test
+	public void numbersAfterWord() {
+		String test = "nickname2021";
+		String regex = "nickname[0-9]+";
+		assertTrue(RegExpMatcher.matches(test, regex));
+	}
+	
+	@Test
+	public void zeroOrOne() {
+		String test = "hello";
+		String regex = "hello?";
+		assertFalse(RegExpMatcher.matches(test, regex));
+	}
+	
+	@Test
+	public void bothEmpty() {
+		String test = "";
+		String regex = "";
+		assertTrue(RegExpMatcher.matches(test, regex));
+	}
+	
+	@Test
+	public void test6() {
+		String test = "123";
+		String regex = "\"123\"";
+		assertTrue(RegExpMatcher.matches(test, regex));
+	}
+	
 	
 }
