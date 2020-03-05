cead07568d4c33956f709d9503f087c82613406b
thomson008 <tkhorszczaruk@gmail.com>
2020-03-05 18:18:05 +0000

    created patch with differences

--- automaton/src/st1920/automaton/REString.java
+++ automaton/src/st1920/automaton/REString.java
@@ -1,12 +1,12 @@
 package st1920.automaton;
 
 public class REString {
-    private String regex = "aaa";
+    private String regex;
 
     public REString(String re) {
         regex = re;
     }
-    
+
     public String getRegex() {
     	return regex;
     }
